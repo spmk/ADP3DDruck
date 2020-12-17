6c02ed91aab15d61014c89e815278fa4af70b32b
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 20:16:16 +0100

    Update execute2.py

--- execute2.py
+++ execute2.py
@@ -13,11 +13,18 @@ run = False
 
 def measure():
     #Erstelle eine neue csv-datei:
-    #date_time = datetime.now().strftime("%y-%m-%d_%H-%M")
-    #path = os.path.dirname(__file__)+"/Data/" + date_time
-    #f = open("Data/" + date_time + ".csv", "w+")
-    #f_csv_writer = csv.writer(f,delimiter=",")
-    #row_index = 0
+    date_time = datetime.now().strftime("%y-%m-%d_%H-%M")
+    path = os.path.dirname(__file__)+"/Data/" + date_time
+    f = open("Data/" + date_time + ".csv", "w+")
+    f_csv_writer = csv.writer(f,delimiter=",")
+    row_index = 0
+    
+    #Scale Ratio setzen
+    scaleRatio = -1 #Spannungswert fuer Warping initial
+    limit = 10000 #Wert, ab dem Warping erkannt wird
+    print("limit zugewiesen")
+    averageOfXValues = 10 #Anzahl an Ausgelesenen Werten, die zur Auswertung gemittelt werden
+    hx711.set_scale_ratio(scaleRatio)
     
     try:
         while run == True:
@@ -25,11 +32,11 @@ def measure():
             print(outputvalue, "") # Hier "" kann eine Einheit eingefuegt werden
 
             #Erstelle Inhalt der naechsten Reihe:
-            #row_time = datetime.now().strftime("%H/%M/%S")
-            #row_content = [row_index, row_time, outputvalue]
-            #row_index +=1
-            #Schreibe die naeste Reihe:
-            #f_csv_writer.writerow(row_content)
+            row_time = datetime.now().strftime("%H/%M/%S")
+            row_content = [row_index, row_time, outputvalue]
+            row_index +=1
+            Schreibe die naeste Reihe:
+            f_csv_writer.writerow(row_content)
         
             if outputvalue > limit:
                 statusLEDs.lightLed("warping")
@@ -40,5 +47,5 @@ def measure():
             else: 
                 statusLEDs.lightLed("no_warping")
     finally:
-        #f.close() # Schliesse Daten.txt
+        f.close() # Schliesse Daten.txt
         GPIO.cleanup()
