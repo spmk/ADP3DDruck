6e7d41c55513e19c38ce921178a5769f3637e5b0
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 14:33:53 +0100

    einfach weil

--- telegrambot.py
+++ telegrambot.py
@@ -20,12 +20,12 @@ def handle(msg):
 	elif command == '/no_warpingLED':
 		bot.sendMessage(chat_id, "Green LED is turned off!")
 		statusLEDs.lightLed("no_warping")
-   	elif command == '/help':
-        	bot.sendMessage(chat_id, "/start - Start the Warping Detective /n /stop - Stop the Warping Detective /n /status - Sends status if warping occured /n /warpingLED - Turns on RED LED /n /no_warpingLED - Turns on GREEN LED"
-  	elif command == '/start'
-       		bot.sendMessage(chat_id, "Ich mach noch nix")
-   	elif command == '/stop'
-       		bot.sendMessage(chat_id, "Ich mach noch nix")
+	elif command == '/help':
+		bot.sendMessage(chat_id, "/start - Start the Warping Detective /n /stop - Stop the Warping Detective /n /status - Sends status if warping occured /n /warpingLED - Turns on RED LED /n /no_warpingLED - Turns on GREEN LED")
+	elif command == '/start':
+		bot.sendMessage(chat_id, "Ich mach noch nix")
+	elif command == '/stop':
+		bot.sendMessage(chat_id, "Ich mach noch nix")
 
 #Bot Objekt wird erstellt und diesem werden die Befehle uebergeben
 bot = telepot.Bot('1405480476:AAHBt_66kwETu0BYK0Y4mtk07t4LtDEVa9c') #Token
