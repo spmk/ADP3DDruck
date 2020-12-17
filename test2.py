70e34459fd4acbd145a7725cc549d2a593c2e059
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 18:44:13 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ from telepot.loop import MessageLoop
 def hello(update: Update, context: CallbackContext) -> None:
     bot.sendMessage("473099318", "Everything is fine :)")
     update.message.reply_text(f'Hello {update.effective_user.Joshua}')
-    
+bot = telepot.Bot('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY')    
 updater = Updater('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
