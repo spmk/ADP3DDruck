6c2f32b084d9df9750a6dfb0a621c5583667672c
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 18:50:12 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,7 +6,6 @@ import execute
 
 def hello(update: Update, context: CallbackContext) -> None:
     bot.sendMessage("473099318", "Everything is fine :)")
-    update.message.reply_text(f'Hello {update.effective_user.Joshua}')
 def start(update: Update, context: CallbackContext) -> None:
     bot.sendMessage(chat_id, "Ich mach noch nix")
     execute.run = True
