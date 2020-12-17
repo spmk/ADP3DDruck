12f2052849a270f693c6ddfde862a6dd773749e0
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 19:35:16 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,6 +3,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 import telepot
 from telepot.loop import MessageLoop
 import execute
+import multiprocessing
 
 chat_id = "473099318"
 Telegram_Token = '1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY'
@@ -26,7 +27,7 @@ x=x+1
 print(x)
 def fire_and_forget(): 
     execute.measure()
-p = execute.measure()
+p = Process(target=fire_and_forget, args=())
 # you have to set daemon true to not have to wait for the process to join
 p.daemon = True
 p.start()
