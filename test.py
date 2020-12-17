e4cc9fde39eae6c552cf7e00e5090ccfd3e203c3
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 18:27:47 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+import os
+
+telegram_token = '1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY'
+
+def start(bot, update)
+    print("kp was ich hier mache")
+    bot.send_message("473099318", "Jo Hoe Hoe")
+
+u = Updater('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY',use_context = True)
+u.start_polling()
+u.idle
