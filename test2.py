7c8846309cd7fc2d2c50f6920f279c0335e18a7e
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 19:04:37 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,26 +5,26 @@ from telepot.loop import MessageLoop
 import execute
 
 chat_id = "473099318"
+Telegram_Token = '1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY'
 
-def hello(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext) -> None:
     bot.sendMessage(chat_id, "Everything is fine :)")
+
+    
+def hello(update: Update, context: CallbackContext) -> None:
+    bot.sendMessage(chat_id, "Ich mach noch nix")
     execute.run = True
     execute.measure()
     
-def start(update: Update, context: CallbackContext) -> None:
-    bot.sendMessage(chat_id, "Ich mach noch nix")
-    #execute.run = True
-    #execute.measure()
-    
 def stop(update: Update, context: CallbackContext) -> None:
     bot.sendMessage(chat_id, "Ich mach noch nix")
-    #execute.run = False
+    execute.run = False
 
 
 
 
-bot = telepot.Bot('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY') #Token
-updater = Updater('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY')
+bot = telepot.Bot(Telegram_Token)
+updater = Updater(Telegram_Token)
 
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
