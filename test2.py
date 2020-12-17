3a709843bf76ebfbd35539c42504184c36387ae2
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 18:49:34 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -15,7 +15,13 @@ def stop(update: Update, context: CallbackContext) -> None:
     bot.sendMessage(chat_id, "Ich mach noch nix")
     execute.run = False
 
+
+
+
+bot = telepot.Bot('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY') #Token
 updater = Updater('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY')
+
+
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
