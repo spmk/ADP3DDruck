220f9c6fb6878f2ece442e29b23711bdbceaaade
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 19:40:13 +0100

    Update execute.py

--- execute.py
+++ execute.py
@@ -12,57 +12,56 @@ import random
 run = False
 #Code wird nur ausgefuhrt, wenn execute direkt ausgefuehrt wird
 def measure(): 
-    if run == True:
-        GPIO.setmode(GPIO.BCM)
-        hx711 = HX711(dout_pin=5,pd_sck_pin=6,
-                        gain_channel_A=64,select_channel='A')
+    GPIO.setmode(GPIO.BCM)
+    hx711 = HX711(dout_pin=5,pd_sck_pin=6,
+                    gain_channel_A=64,select_channel='A')
 
-        #hx711.reset()   #Before we start, reset the HX711 (not obligate)
-        #hx711.zero()    #Offset eliminieren
+    #hx711.reset()   #Before we start, reset the HX711 (not obligate)
+    #hx711.zero()    #Offset eliminieren
 
-        #Scale Ratio setzen
-        scaleRatio = -1 #Spannungswert fuer Warping initial
-        limit = 10000 #Wert, ab dem Warping erkannt wird
-        averageOfXValues = 10 #Anzahl an Ausgelesenen Werten, die zur Auswertung gemittelt werden
-        hx711.set_scale_ratio(scaleRatio)
+    #Scale Ratio setzen
+    scaleRatio = -1 #Spannungswert fuer Warping initial
+    limit = 10000 #Wert, ab dem Warping erkannt wird
+    averageOfXValues = 10 #Anzahl an Ausgelesenen Werten, die zur Auswertung gemittelt werden
+    hx711.set_scale_ratio(scaleRatio)
 
-        #Erstelle eine neue csv-datei:
-        date_time = datetime.now().strftime("%y-%m-%d_%H-%M")
-        path = os.path.dirname(__file__)+"/Data/" + date_time
-        f = open("Data/" + date_time + ".csv", "w+")
-        f_csv_writer = csv.writer(f,delimiter=",")
-        row_index = 0
+    #Erstelle eine neue csv-datei:
+    date_time = datetime.now().strftime("%y-%m-%d_%H-%M")
+    path = os.path.dirname(__file__)+"/Data/" + date_time
+    f = open("Data/" + date_time + ".csv", "w+")
+    f_csv_writer = csv.writer(f,delimiter=",")
+    row_index = 0
 
-        #print("Values are saved to: " + date_time + ".csv")
+    #print("Values are saved to: " + date_time + ".csv")
 
-        #Gewichte ausgeben, LEDs Relaise ansteuern
-        print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
-        print('Current value measured is: ')
+    #Gewichte ausgeben, LEDs Relaise ansteuern
+    print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
+    print('Current value measured is: ')
 
-        try:	
-            while run == True:
-                outputvalue = random.random()
-                print(outputvalue, "") # Hier "" kann eine Einheit eingefuegt werden
+    try:	
+        while run == True:
+            outputvalue = random.random()
+            print(outputvalue, "") # Hier "" kann eine Einheit eingefuegt werden
 
-                #Erstelle Inhalt der naechsten Reihe:
-                row_time = datetime.now().strftime("%H/%M/%S")
-                row_content = [row_index, row_time, outputvalue]
-                row_index +=1
-                #Schreibe die naeste Reihe:
-                f_csv_writer.writerow(row_content)
-            
-                if outputvalue>limit:
-                        statusLEDs.lightLed("warping")
-                        Relais.statusDrucker("warping")
-                        telegrambot.sendMessage()
-                        time.sleep(20)
-                        Relais.statusDrucker("no_warping")
-                else: 
-                        statusLEDs.lightLed("no_warping")
+            #Erstelle Inhalt der naechsten Reihe:
+            row_time = datetime.now().strftime("%H/%M/%S")
+            row_content = [row_index, row_time, outputvalue]
+            row_index +=1
+            #Schreibe die naeste Reihe:
+            f_csv_writer.writerow(row_content)
+        
+            if outputvalue>limit:
+                    statusLEDs.lightLed("warping")
+                    Relais.statusDrucker("warping")
+                    telegrambot.sendMessage()
+                    time.sleep(20)
+                    Relais.statusDrucker("no_warping")
+            else: 
+                    statusLEDs.lightLed("no_warping")
 
-        except (KeyboardInterrupt, SystemExit): #Programm kann mit Ctrl + C angehalten werden
-            print("Pfiat di Gott! :D")
+    except (KeyboardInterrupt, SystemExit): #Programm kann mit Ctrl + C angehalten werden
+        print("Pfiat di Gott! :D")
 
-        finally:
-            f.close() # Schliesse Daten.txt
-            GPIO.cleanup()
+    finally:
+        f.close() # Schliesse Daten.txt
+        GPIO.cleanup()
