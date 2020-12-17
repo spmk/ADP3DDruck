7e6b967989ecd043126950c776a59c7049838fd1
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 20:21:42 +0100

    Update execute2.py

--- execute2.py
+++ execute2.py
@@ -16,16 +16,16 @@ def measure():
     hx711 = HX711(dout_pin=5,pd_sck_pin=6,
                     gain_channel_A=64,select_channel='A')
     
-    #Erstelle eine neue csv-datei:
+    # Erstelle eine neue csv-datei:
     date_time = datetime.now().strftime("%y-%m-%d_%H-%M")
     path = os.path.dirname(__file__)+"/Data/" + date_time
     f = open("Data/" + date_time + ".csv", "w+")
     f_csv_writer = csv.writer(f,delimiter=",")
     row_index = 0
     
-    #Scale Ratio setzen
-    scaleRatio = -1 #Spannungswert fuer Warping initial
-    limit = 10000 #Wert, ab dem Warping erkannt wird
+    # Scale Ratio setzen
+    scaleRatio = -1 # Spannungswert fuer Warping initial
+    limit = 10000 # Wert, ab dem Warping erkannt wird
     print("limit zugewiesen")
     averageOfXValues = 10 #Anzahl an Ausgelesenen Werten, die zur Auswertung gemittelt werden
     hx711.set_scale_ratio(scaleRatio)
@@ -35,11 +35,11 @@ def measure():
             outputvalue = random.random()
             print(outputvalue, "") # Hier "" kann eine Einheit eingefuegt werden
 
-            #Erstelle Inhalt der naechsten Reihe:
+            # Erstelle Inhalt der naechsten Reihe:
             row_time = datetime.now().strftime("%H/%M/%S")
             row_content = [row_index, row_time, outputvalue]
             row_index +=1
-            #Schreibe die naeste Reihe:
+            # Schreibe die naeste Reihe:
             f_csv_writer.writerow(row_content)
         
             if outputvalue > limit:
