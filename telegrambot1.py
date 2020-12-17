8f64c6790e25fcdfea2a207f54b54cd4aa962b47
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 17:42:54 +0100

    Create telegrambot1.py

---
+++ telegrambot1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import telepot
+import time
+from telepot.loop import MessageLoop
+#import statusLEDs
+#import execute
+x=0
+#Hier sind alle Befehle definiert, die der Bot ausfuehren kann.
+def handle(msg):
+	chat_id = msg['chat']['id']
+	command = msg['text']
+	print('Got command: %s' % command)
+	
+	if command == '/status':
+		bot.sendMessage(chat_id, "Everything is fine :)")
+	elif command == '/warpingLED':
+		bot.sendMessage(chat_id, "Red LED is turned on!")
+#		statusLEDs.lightLed("warping")
+	elif command == '/no_warpingLED':
+		bot.sendMessage(chat_id, "Green LED is turned off!")
+#		statusLEDs.lightLed("no_warping")
+	elif command == '/help':
+		bot.sendMessage(chat_id, "/start - Start the Warping Detective \n /stop - Stop the Warping Detective \n /status - Sends status if warping occured \n /warpingLED - Turns on RED LED \n /no_warpingLED - Turns on GREEN LED")
+	elif command == '/start':
+		bot.sendMessage(chat_id, "Ich mach noch nix")
+#		execute.run = True
+#		execute.measure()
+	elif command == '/stop':
+		bot.sendMessage(chat_id, "Ich mach noch nix")
+#		execute.run = False
+		
+
+#Bot Objekt wird erstellt und diesem werden die Befehle uebergeben
+bot = telepot.Bot('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY') #Token
+MessageLoop(bot, handle).run_as_thread()
+
+#Boot up Nachricht des Bots:
+print("Hi! I am your personal warping assistant!") #Auf der Konsole
+bot.sendMessage("473099318", "Hi I am your personal warping assistent!") #In Telegram
+bot.sendMessage("473099318", "I will text you if warping occurs.") #In Telegram
+x=x+1
+print(x)
+#Diese Methode wird in execute.py aufgerufen, wenn Warping auftritt. 
+def sendMessage():
+	bot.sendMessage("473099318", "Warping erkannt und Drucker abgeschaltet!")
