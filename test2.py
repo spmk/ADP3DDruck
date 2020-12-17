c07f4ff70d56f06822cf7efb424960b75fb5b831
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 18:48:42 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -15,7 +15,7 @@ def stop(update: Update, context: CallbackContext) -> None:
     bot.sendMessage(chat_id, "Ich mach noch nix")
     execute.run = False
 
-
+updater = Updater('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY')
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
