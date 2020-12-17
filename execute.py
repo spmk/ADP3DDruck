07ae38427433924da2f5071d4fe17384b9172579
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 17:07:18 +0100

    Update execute.py

--- execute.py
+++ execute.py
@@ -51,28 +51,28 @@ def measure():
         GPIO.cleanup()
     
     try:	
-		while run == True:
-			outputvalue = random.random()
-			print(outputvalue, "") # Hier "" kann eine Einheit eingefuegt werden
+	while run == True:
+		outputvalue = random.random()
+		print(outputvalue, "") # Hier "" kann eine Einheit eingefuegt werden
 
-			#Erstelle Inhalt der naechsten Reihe:
-			#row_time = datetime.now().strftime("%H/%M/%S")
-			#row_content = [row_index, row_time, outputvalue]
-			#row_index +=1
-			#Schreibe die naeste Reihe:
-			#f_csv_writer.writerow(row_content)
-
-			if outputvalue>limit:
-				statusLEDs.lightLed("warping")
-				Relais.statusDrucker("warping")
-				telegrambot.sendMessage()
-				time.sleep(20)
-				Relais.statusDrucker("no_warping")
-			else: 
-				statusLEDs.lightLed("no_warping")
-
-			if run == False:
-				break
+		#Erstelle Inhalt der naechsten Reihe:
+		#row_time = datetime.now().strftime("%H/%M/%S")
+		#row_content = [row_index, row_time, outputvalue]
+		#row_index +=1
+		#Schreibe die naeste Reihe:
+		#f_csv_writer.writerow(row_content)
+		
+		if outputvalue>limit:
+			statusLEDs.lightLed("warping")
+			Relais.statusDrucker("warping")
+			telegrambot.sendMessage()
+			time.sleep(20)
+			Relais.statusDrucker("no_warping")
+		else: 
+			statusLEDs.lightLed("no_warping")
+		
+		if run == False:
+			break
 	
     except (KeyboardInterrupt, SystemExit): #Programm kann mit Ctrl + C angehalten werden
 		print("Pfiat di Gott! :D")
