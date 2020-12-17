2fa22d43f4a66fe634c0b6f51384e143476eba54
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 19:16:08 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -10,7 +10,7 @@ Telegram_Token = '1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY'
 def hello(update: Update, context: CallbackContext) -> None:
     bot.sendMessage(chat_id, "Everything is fine :)")
     
-def starten(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext) -> None:
     bot.sendMessage(chat_id, "Ich mach noch nix")
     execute.run = True
     execute.measure()
@@ -23,6 +23,9 @@ def stop(update: Update, context: CallbackContext) -> None:
 bot = telepot.Bot(Telegram_Token)
 updater = Updater(Telegram_Token)
 
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("stop", stop))
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
