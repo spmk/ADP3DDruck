94f454b5f32fde361bb6edacc7cac741017dc796
spmk <30893752+spmk@users.noreply.github.com>
2020-12-17 18:36:31 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,9 +4,11 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
-
-updater = Updater('YOUR TOKEN HERE')
+    bot.sendMessage('473099318', "i work!")
+    
+    
+bot.sendMessage('473099318', "Everything is fine :)")
+updater = Updater('1435246331:AAEuTzd96pMR8ACXl92za8CSFo_0gd1QCvY')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
