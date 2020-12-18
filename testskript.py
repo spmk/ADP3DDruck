f70e8c70ed9529ac8e018b9f7784263158815de9
spmk <30893752+spmk@users.noreply.github.com>
2020-12-18 11:48:43 +0100

    Update testskript.py

--- testskript.py
+++ testskript.py
@@ -1,7 +1,7 @@
 import time
 import RPi.GPIO as GPIO
 # Zählweise der Pins festlegen
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 # Pin 22 (GPIO 25) als Ausgang festlegen
 GPIO.setup(5, GPIO.IN)
 GPIO.setup(6, GPIO.IN)
